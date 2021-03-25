6c9373125286bbfa064e6257e97b756f3ac34c96
Jingbo Fan <flyingpot@macbook.local>
2021-03-26 00:19:14 +0800

    convert ascii art to png

--- api.go
+++ api.go
@@ -1,24 +1,53 @@
 package main
 
 import (
+	"io/ioutil"
+	"log"
 	"net/http"
+	"strings"
 
-	"github.com/gin-gonic/gin"
+	"github.com/golang/freetype"
+
+	"github.com/hqbobo/text2pic"
 )
 
 func main() {
+	http.Handle("/", http.HandlerFunc(generateSvg))
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Fatal("ListenAndServe:", err)
+	}
+}
 
-	r := gin.Default()
-
-	r.GET("/api", func(c *gin.Context) {
-		username := c.DefaultQuery("username", "flyingpot")
-		info, error := fetch(username)
-		if error != nil {
-			c.AbortWithStatus(http.StatusBadGateway)
-			return
+func generateSvg(w http.ResponseWriter, req *http.Request) {
+	query := req.URL.Query()
+	usernames, present := query["username"]
+	var username string
+	if !present || len(usernames) == 0 {
+		username = "flyingpot"
+	}
+	info, error := fetch(username)
+	if error != nil {
+		http.Error(w, "get stats error", http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", "image/png")
+	fontBytes, err := ioutil.ReadFile("font/Courier Prime.ttf")
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	f, err := freetype.ParseFont(fontBytes)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	pic := text2pic.NewTextPicture(text2pic.Configure{Width: 1500, BgColor: text2pic.ColorWhite})
+	for _, line := range strings.Split(render(info), "\n") {
+		if len(line) == 0 {
+			continue
 		}
-		c.String(200, render(info))
-	})
-
-	r.Run()
+		pic.AddTextLine(line, 8, f, text2pic.ColorBlack, text2pic.Padding{LineSpace: -15})
+	}
+	pic.Draw(w, text2pic.TypePng)
 }
